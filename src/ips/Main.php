6fc8135bacb59f73b93c02c33110c16b8d97ce96
RexZetronYT23 <57451491+RexZetronYT23@users.noreply.github.com>
2020-08-02 23:53:18 +0200

    Update Main.php

--- src/ips/Main.php
+++ src/ips/Main.php
@@ -3,16 +3,33 @@
 
 namespace ips;
 
-use pocketmine\pluuginPluginBase;
+use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\{
     Server,
     Player
 };
+use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
 
-    
+    public function onEnable(){
+           $this->getLogger()->info("§aSuccesfully loaded IPLogger!");
+           $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function login(PlayerPreLoginEvent $ev){
+
+           if (!file_exists($this->getDataFolder() . $ev->getPlayer()->getName() . ".yml') {
+
+               $c = new Config($this->getDataFolder() . $ev->getPlayer()->getName() . ".yml", Config::YAML);
+
+               $c->set("IP", $ev->getPlayer()->getAddress());
+
+               $c->save();
+           }
+
+    }
 
 }
