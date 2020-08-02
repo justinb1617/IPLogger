4cedc7c60a2fd44412a255c05fd149348495a3f8
RexZetronYT23 <57451491+RexZetronYT23@users.noreply.github.com>
2020-08-02 23:47:06 +0200

    Update Main.php

--- src/ips/Main.php
+++ src/ips/Main.php
@@ -1,1 +1,18 @@
 
+<?php
+
+namespace ips;
+
+use pocketmine\pluuginPluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\{
+    Server,
+    Player
+};
+
+class Main extends PluginBase implements Listener{
+
+    
+
+}
